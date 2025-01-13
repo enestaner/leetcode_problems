ce517ef890ba368c622b7b31dc0cf35a4457eec7
enestaner <enestaner963@gmail.com>
2025-01-13 12:30:02 +0300

    quick-fix

--- easy/217.cpp
+++ easy/217.cpp
@@ -11,11 +11,12 @@ Output: true
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
+        //If the size of the array is 1, then return false
+        if(nums.size() <= 1) return false;
+
         //Sort the array
         sort(nums.begin(), nums.end());
 
-        //If the size of the array is 1, then return false
-        if(nums.size() == 1) return false;
 
         //Iterate through the array and check if the element is equal to the next element or not
         for(int i = 0; i < nums.size()-1; i++){
